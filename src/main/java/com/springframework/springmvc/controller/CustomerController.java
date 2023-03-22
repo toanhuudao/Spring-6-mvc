e985c7229f4b42a3417c7afaefdbc03695bed853
toandh <toandh.thuylinh@gmail.comT>
2023-03-23 06:59:26 +0700

    init customer controller

---
+++ src/main/java/com/springframework/springmvc/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.springframework.springmvc.controller;
+
+import com.springframework.springmvc.models.Beer;
+import com.springframework.springmvc.models.Customer;
+import com.springframework.springmvc.services.BeerService;
+import com.springframework.springmvc.services.CustomerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/beer")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomer(){
+        log.debug("Get list customers- in controller");
+        return customerService.listCustomers();
+    }
+    @RequestMapping(value = "/{customerId}", method = RequestMethod.GET)
+    Customer getCustomerById(@PathVariable("customerId") UUID customerId){
+        log.debug("Get customer by Id - in controller");
+        return customerService.getCustomerById(customerId);
+    }
+}
