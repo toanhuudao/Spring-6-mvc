871345abb6618d12f07c3f9a1042bf44c68f8f35
toandh <toandh.thuylinh@gmail.comT>
2023-03-23 23:44:27 +0700

    init test mokito

--- src/main/java/com/springframework/springmvc/controller/BeerController.java
+++ src/main/java/com/springframework/springmvc/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.springframework.springmvc.controller;
 import com.springframework.springmvc.models.Beer;
 import com.springframework.springmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
--- src/test/java/com/springframework/springmvc/controller/BeerControllerTest.java
+++ src/test/java/com/springframework/springmvc/controller/BeerControllerTest.java
@@ -1,20 +1,54 @@
 package com.springframework.springmvc.controller;
 
+import com.springframework.springmvc.models.Beer;
+import com.springframework.springmvc.services.BeerService;
+import com.springframework.springmvc.services.BeerServiceImpl;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
+//    @Autowired
+//    BeerController beerController;
+
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+//        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+
+        Beer testBeer = beerServiceImpl.listBeer().get(0);
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/"+UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName",is(testBeer.getBeerName())));
     }
 }
